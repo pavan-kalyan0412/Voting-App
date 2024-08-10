cd3549f375f9d05452acee4bab4fd99e6b641475
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-08-10 11:42:11 +0530

    added some more condition to models

--- models/candidate.js
+++ models/candidate.js
@@ -6,13 +6,32 @@ const candidateSchema = new mongoose.Schema({
         type: String,
         required: true
     }, 
-    Party:{
+    party:{
         type: String,
         required: true
     },
     age:{
         required: true,
-        type: Number
+        type: Number,
+        min: 18,  // Assuming 18 is the minimum age for a candidate
+        max: 100  // Assuming 100 is the maximum age for a candidate
+    },
+    votes:[
+        {
+            user:{
+                type:mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            votedAt:{
+                type:Date,
+                default: Date.now
+            }
+        }
+    ],
+    voteCount:{
+        type: Number,
+        default: 0
     }
 });
 
