69f658d235d80e026288a00b84133a75dc11398e
Pavan Kalyan <pavankalyan.yes@gmail.com>
2024-08-13 14:00:10 +0530

    delete candidate by admin only

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -59,7 +59,24 @@ router.put('/update-candidate/:id', authenticateToken, isAdmin, async(req,res)=>
             res.status(500).json({ error:'internal server error'})
     }
 })
+// Route to delete a candidate
+router.delete('/delete-candidate/:id', authenticateToken, isAdmin, async (req, res) => {
+    try {
+        const { id } = req.params;
 
+        // Find the candidate by ID and delete
+        const deletedCandidate = await Candidate.findByIdAndDelete(id);
+
+        if (!deletedCandidate) {
+            return res.status(404).json({ error: 'Candidate not found' });
+        }
+
+        res.status(200).json({ message: 'Candidate deleted successfully' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 
 module.exports = router;
